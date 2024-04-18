6175c6d70ab53d5f4b5e9685390d764c0d46e386
Lutfullo1 <ismoilovlutfullo1117@gmail.com>
2024-04-19 01:16:04 +0500

    Liber

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="container">
-      <Link href={`/users`}>Profile</Link>
+      <Link className="underline text-[24px] text-blue-600" href={`/users`}>
+        Profile
+      </Link>
     </div>
   );
 }
